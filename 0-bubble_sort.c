d617b56b34f0c399ee89db6aa6ab38b33b93c5cb
omar546 <omer.mkawy.om@gmail.com>
2023-11-21 23:43:10 +0200

    g

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,33 +9,29 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, indx;
-	int temp;
-	int swapped;  // a variable to track whether any swaps occurred
+size_t i, indx;
+int temp;
+int swapped;
 
-	if (size < 2)
-		return;
+if (size < 2)
+return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		// Optimization: If no swaps occur in a pass, the array is already sorted
-		swapped = 0;
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
 
-		for (indx = 0; indx < size - i - 1; indx++)
-		{
-			// Fixing the inner loop condition and array bounds check
-			if (array[indx] > array[indx + 1])
-			{
-				temp = array[indx];
-				array[indx] = array[indx + 1];
-				array[indx + 1] = temp;
-				print_array(array, size);
-				swapped = 1;  // Set swapped to true if a swap occurs
-			}
-		}
-
-		// If no swaps occurred, the array is already sorted, and we can exit early
-		if (swapped == 0)
-			break;
-	}
+for (indx = 0; indx < size - i - 1; indx++)
+{
+if (array[indx] > array[indx + 1])
+{
+temp = array[indx];
+array[indx] = array[indx + 1];
+array[indx + 1] = temp;
+print_array(array, size);
+swapped = 1;
+}
+}
+if (swapped == 0)
+break;
+}
 }
