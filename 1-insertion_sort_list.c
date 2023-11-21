43b39c3b6d74737bde651e30d31bb19d968a6819
omar546 <omer.mkawy.om@gmail.com>
2023-11-22 00:17:12 +0200

    s

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,19 +8,23 @@
 
 void insertion_sort_list(listint_t **list)
 {
-listint_t *now, *temp;
+listint_t *node;
 
-if (list && *list && (*list)->next)
+if (list == NULL || (*list)->next == NULL)
+return;
+
+node = (*list)->next;
+while (node)
 {
-for (now = (*list)->next; now; now = now->next)
+while (node->prev && node->prev->n > node->n)
 {
-temp = now;
-while (temp->prev && now->n < temp->prev->n)
-swapp_list(*list, temp->prev, temp);
-}
-}
+node = swap_node(node, list);
+print_list(*list);
 }
 
+node = node->next;
+}
+}
 
 /**
  * swap_node - Swap a node with its previous one in a doubly linked list.
