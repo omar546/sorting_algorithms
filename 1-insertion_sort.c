4b5df84dacf2af56eaf873c46d7b2eec4ab6520c
omar546 <omer.mkawy.om@gmail.com>
2023-11-21 23:53:05 +0200

    d

--- 1-insertion_sort.c
+++ 1-insertion_sort.c
@@ -8,22 +8,22 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node;
+listint_t *node;
 
-    if (list == NULL || (*list)->next == NULL)
-        return;
+if (list == NULL || (*list)->next == NULL)
+return;
 
-    node = (*list)->next;
-    while (node)
-    {
-        while (node->prev && node->prev->n > node->n)
-        {
-            node = swap_node(node, list);
-            print_list(*list);
-        }
+node = (*list)->next;
+while (node)
+{
+while (node->prev && node->prev->n > node->n)
+{
+node = swap_node(node, list);
+print_list(*list);
+}
 
-        node = node->next;
-    }
+node = node->next;
+}
 }
 
 /**
@@ -34,23 +34,23 @@ void insertion_sort_list(listint_t **list)
  */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
-    listint_t *prev = node->prev;
+listint_t *prev = node->prev;
 
-    if (prev)
-    {
-        prev->next = node->next;
-        if (node->next)
-            node->next->prev = prev;
+if (prev)
+{
+prev->next = node->next;
+if (node->next)
+node->next->prev = prev;
 
-        node->next = prev;
-        node->prev = prev->prev;
-        prev->prev = node;
+node->next = prev;
+node->prev = prev->prev;
+prev->prev = node;
 
-        if (node->prev)
-            node->prev->next = node;
-        else
-            *list = node;
-    }
+if (node->prev)
+node->prev->next = node;
+else
+*list = node;
+}
 
-    return node;
+return node;
 }
